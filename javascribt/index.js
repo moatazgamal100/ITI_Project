f4b16c55b307742861ee9703ef0ae026efee09d6
moataz <moatazgamal100100@gmail.com>
2023-12-01 06:43:25 -0800

    new edit to javascribt file

--- javascribt/index.js
+++ javascribt/index.js
@@ -96,17 +96,89 @@
 //     console.log(x);
 // })();
 
-var firstName = window.prompt("enter your first name");
+// var firstName = window.prompt("enter your first name");
 
-var lastName = window.prompt("enter your last name");
+// var lastName = window.prompt("enter your last name");
 
-var fullName = firstName +" "+ lastName;
-console.log("my full name is " +fullName);
+// var fullName = firstName +" "+ lastName;
+// console.log("my full name is " +fullName);
 
 
-var num1 = window.prompt("enter your first num");
+// var num1 = window.prompt("enter your first num");
 
-var num2 = window.prompt("enter your second num");
+// var num2 = window.prompt("enter your second num");
 
-var result = Number(num1) + Number(num2);
-console.log(num1 +" + "+ num2 +" = "+result);
+// var result = Number(num1) + Number(num2);
+// console.log(num1 +" + "+ num2 +" = "+result);
+// var x = 1;
+// switch (x) {
+//     case 1:
+//         alert("one");
+//     case 2:
+//         alert("two");
+// }
+
+// var r = null;
+// alert(typeof (r));
+
+//number one-------------------------------------
+var age;
+function getcheck() {
+    var check = window.confirm("do you want to add another age ?");
+        if (check) {
+            getage(age);
+        }
+        else {
+            alert("done");
+        }
+}
+function getage(agee) {
+    var age = Number(window.prompt("enter your age"));
+    if (age < 0) {
+        console.log("your input is wrong");
+    }
+    else if (age >= 1 && age <= 10) {
+        alert("the status of the user is Child");
+        getcheck()
+    }
+    else if (age >= 11 && age <= 18) {
+        alert("the status of the user is Teenager");
+        getcheck();
+    }
+    else if (age >= 19 && age <= 50) {
+        alert("the status of the user is Grown Up");
+        getcheck();
+    }
+    else if ( age > 50) {
+        alert("the status of the user is Old");
+        getcheck();
+    }
+}
+getage(age);
+// //finish number one-----------------------------------
+
+//number two-----------------------------------------------
+var namee = window.prompt("enter string");
+var count = 0;
+for (var i = 0; i < namee.length; i++){
+    if (namee.charAt(i) == "a" || namee.charAt(i) == "e" || namee.charAt(i) == "o" || namee.charAt(i) == "u" || namee.charAt(i) == "i") {
+        count++;
+    }
+    
+}
+alert("the count of vowels in string is "+count);
+// //finish number two-----------------------------------
+
+var clock = Number(window.prompt("enter hour clock"));
+if (clock > 24 || clock < 0 ) {
+    alert("wrong hour clock");
+}
+else {
+    if (clock >= 0 && clock <= 12) {
+        alert(clock +" AM");
+    }
+    else if (clock >12 && clock <= 24) {
+        alert((clock-12) +" PM");   
+    }
+}
+    
