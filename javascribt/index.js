1e5fcec96cb40f575aa2f5ae28c90243752c3591
moatazgamal100 <moatazfahmy100100@gmail.com>
2023-12-13 18:42:14 -0800

    lap 5

--- javascribt/index.js
+++ javascribt/index.js
@@ -1,214 +1,154 @@
-///////JavaScript lap 4 OOP////////////////////////////
-////////Constructor function///////////////////////////////
-
-
- // Constructor function
-function Person1(fullName, money, sleepMood, healthRate)
-{
-
-this.fullName = fullName;
-this.money = money;
-this.sleepMood = sleepMood;
-this.healthRate = healthRate;
-
-this.sleep = function(sleepHours) {
-
-    if (sleepHours = 7){
-        this.sleepMood = "happy";
+class person { 
+    constructor(full_name, mony, sleep_mood, health_rate) {
+        this.full_name = full_name;
+        this.mony = mony;
+        this.sleep_mood = sleep_mood;
+        this.health_rate = health_rate;
     }
-    else if (sleepHours < 7)
-    {
-        this.sleepMood="tired";
-    }
-    else{
-        this.sleepMood="lazy";
-    }
-
-    return sleepMood;
-};
-
-this.eat = function(meals) {
-    switch (meals){
-
-        case 3 : this.healthRate = 100
-        break;
-        case 2 : this.healthRate = 75
-        break;
-        case 1 : this.healthRate = 50 
-        break;
-    }
-    return healthRate;
-    };
-
-this.buy = function(item) {
-    
-        item = parseInt(item);
-    
-};
-}
-
-var Moataz = new Person1('moataz gamal', 100, "lazy", 100);
-// TEST
-Moataz.sleep(8);
-Moataz.eat(3);
-Moataz.buy(1);
-// TEST
-console.log(Moataz.money);
-console.log(Moataz.healthRate);
-console.log(Moataz.fullName);
-console.log(Moataz.sleepMood);
-///////////////////////////////////////////////////////////////////
-// Problem Solved Using →→→  classes
-// Class definition
-class Person2 {
-    constructor(fullName, money, sleepMood, healthRate) {
-        // Attributes
-        this.fullName = fullName;
-        this.money = money;
-        this.sleepMood = sleepMood;
-        this.healthRate = healthRate;
-    }
-
-    // Methods
-    sleep(sleepHours) {
-        if (sleepHours === 7) {
-            this.sleepMood = 'happy';
-        } else if (sleepHours < 7) {
-            this.sleepMood = 'tired';
-        } else {
-            this.sleepMood = 'lazy';
+    sleep (hours) {
+        if (hours == 7) {
+            this.sleep_mood = "happy"
         }
-
-        return this.sleepMood;
-    }
-
-    eat(meals) {
-        switch (meals) {
-            case 3:
-                this.healthRate = 100;
-                break;
-            case 2:
-                this.healthRate = 75;
-                break;
-            case 1:
-                this.healthRate = 50;
-                break;
+        else if (hours > 7) {
+            this.sleep_mood = "lazy"
         }
-        return this.healthRate;
+        else if (hours < 7) {
+            this.sleep_mood = "tired"
+        }
+        return this.sleep_mood
     }
+    eat (meals) {
+        let heal = 0;
+        if (meals == 3) {
+            heal = 100;
+            this.health_rate = heal ;
+        }
+        else if (meals == 2) {
+            heal = 75;
+            this.health_rate = heal ;
+        }
+        else if (meals == 1) {
+            heal = 50;
+            this.health_rate = heal;
+        }
+        return this.health_rate+ " health rate";
+    }
+    buy (items) {
+        return this.mony - (items * 10);
+    }
+} 
 
-    buy(item) {
-        item = parseInt(item);
-        this.money -= item * 10;
+class Employee extends person{
+    constructor(full_name, mony, sleep_mood, health_rate,id,email,workMood,salary,isManger) {
+        super(full_name, mony, sleep_mood, health_rate)
+        this.id = id;
+        this.email = email;
+        this.workMood = workMood;
+        this.salary = (salary < 1000) ? 1000:salary;
+        this.isManger = isManger;
+    }
+    work(hours) {
+        if (hours == 8) {
+            this.workMood = "happy"
+        }
+        else if (hours > 8) {
+            this.workMood = "tired"
+        }
+        else if (hours < 8) {
+            this.workMood = "lazy"
+        }
+        return this.workMood
     }
 }
 
-const moataz = new Person2('Moataz gamal', 100, 'lazy', 100);
+let moataz = new Employee("ahmed gamal", 100, "mood", " health rate", 1, "moatazgamal100100@gmail.com", "work", 10000, "mngr");
 
-// TEST
-moataz.sleep(8);
-moataz.eat(3);
-moataz.buy(1);
+console.log(moataz.buy(2));
+console.log(moataz.eat(2));
+console.log(moataz.sleep(5));
+console.log(moataz.work(5));
 
-//TEST
-console.log(moataz.money);
-console.log(moataz.healthRate);
-console.log(moataz.fullName);
-console.log(moataz.sleepMood);
-///////////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////////////
-const Person3 = {
-    init(fullName, money, sleepMood, healthRate) {
-        this.fullName = fullName;
-        this.money = money;
-        this.sleepMood = sleepMood;
-        this.healthRate = healthRate;
-    },
-
-    sleep(hours) {
-        if (hours === 7) {
-        this.sleepMood = "happy";
-        } else if (hours < 7) {
-        this.sleepMood = "tired";
-        } else if (hours > 7) {
-        this.sleepMood = "lazy";
+class Office{
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+    getAllEmployees() {
+        return this.employees;
+    }
+    getEmployee(empid) {
+        var returnElement = this.employees.find(function (ele) {
+            if (ele.id === empid) {
+                return true;
+                
+            }
+            else{
+                return false;
+            }
+        })
+        if (returnElement.isManger === true) {
+            
+            delete returnElement.salary
+            return returnElement;
+            
         }
-    },
-
-    eat(meals) {
-        if (meals = 3) {
-        this.healthRate = 100;
-        } else if (meals = 2) {
-        this.healthRate = 75;
-        } else  (meals = 1) 
-        this.healthRate = 50;
+        else if (returnElement.isManger === false) {
+            return returnElement;
+        }
+    }
+    hire(Employee) {
+        this.employees.push(Employee);
+    }
+    fire(empid) {
+        this.employees = this.employees.filter(function (em) {
+            em.id !== empid;
+        })
+    }
+}
+let off = new Office('ITI');
+let arr=[]
+while (true) {
+    let addCondition = confirm("Do you need to add new employee")
+    while (addCondition == true) {
+        let action = prompt('Enter your action ( mngr, nrml, q):');
+        let ismanger = false;
+        if (action == "mngr") {
+            ismanger = true;
+        }
         
-    },
+        if (action === 'q') {
+            break;
+        }
 
-    buy(items) {
-        var itemCost = 10 * items;
+        let name = prompt('Enter your Name:');
+        let id = prompt('ID :');
+        arr.push(id);
+        let email = prompt('Email :');
+        let salary = parseInt(prompt('Salary:'));
+        let emp = new Employee(name, 100, "mood", " health rate", id,email, "work", salary, ismanger);
+
+
+        off.hire(emp);
+        
+    }
+    if (addCondition == false) {
+        break
+    }
     
-        this.money -= itemCost;
+    
+}
 
+console.log(off.getAllEmployees());
+
+let empId = prompt('Enter employee ID to display details:');
+let check=0
+for (let empIddd of arr) {
+    if (empIddd == empId) {
+        let employeeDetails = off.getEmployee(empId);
+        console.log(employeeDetails);
+        check = 1;
     }
-
-    };
-
-// TEST
-moataz.sleep(8);
-moataz.eat(3);
-moataz.buy(1);
-
-//TEST
-console.log(moataz.money);
-console.log(moataz.healthRate);
-console.log(moataz.fullName);
-console.log(moataz.sleepMood);
-/////////////////////////////////////////////////////////////////////////////////
- //////////////////////////////////////////////////////////////////////
-function createPerson(fullName, money, sleepMood, healthRate) {
-    var person = {
-        fullName,
-        money,
-        sleepMood,
-        healthRate,
-
-        sleep(hours) {
-        if (hours = 7) {
-            this.sleepMood = "happy";
-        } else if (hours < 7) {
-            this.sleepMood = "tired";
-        } else {
-            this.sleepMood = "lazy";
-        }
-        },
-
-        eat(meals) {
-        if (meals = 3) {
-            this.healthRate = 100;
-        } else if (meals = 2) {
-            this.healthRate = 75;
-        } else (meals = 1) 
-            this.healthRate = 50;
-        
-        },
-
-        buy(items) {
-        this.money -= items * 10;
-        },
-    };
-
-    return person;
-    }
-
-// TEST
-moataz.sleep(8);
-moataz.eat(3);
-moataz.buy(1);
-
-//TEST
-console.log(moataz.money);
-console.log(moataz.healthRate);
-console.log(moataz.fullName);
-console.log(moataz.sleepMood);
+}
+if (check == 0) {
+        console.log('Employee not found.');
+}
